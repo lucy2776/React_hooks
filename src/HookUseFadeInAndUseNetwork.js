9d45f4ac820e70e4ebc01eb867b7f869db0436e5
lucy2776 <lucy2776@naver.com>
2023-04-11 02:33:55 +0900

    useNetwork

--- src/HookUseFadeInAndUseNetwork.js
+++ src/HookUseFadeInAndUseNetwork.js
@@ -40,9 +40,10 @@ export default HookUseFadeIn
 const useNetwork = (onChange) => {
     const [status, setStatus] = useState(navigator.onLine)
     const handleChange = () => {
-        if (typeof onChange !== "function") {
-            return
-        }
+        if (typeof onChange === "function") {
+            onChange(navigator.onLine)
+        } // console용 ?
+
         setStatus(navigator.onLine)
     }
 
