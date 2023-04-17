f5fc846950aef69c30b6417865e3ebadd0dbaf70
lucy2776 <lucy2776@naver.com>
2023-04-17 21:41:25 +0900

    useAxios

--- src/HookUseAxios.js
+++ src/HookUseAxios.js
@@ -1,12 +1,51 @@
-const useAxios = () => {
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 
+const defaultAxios = axios.create({
+    baseURL: 'https://jsonplaceholder.typicode.com',
+});
+
+const useAxios = (opts, axiosInstance = defaultAxios) => {
+    const [state, setState] = useState({
+        loading: true,
+        error: null,
+        data: null
+    })
+
+    useEffect(() => {
+        if (!opts.url) {
+            return
+        }
+
+        axiosInstance(opts)
+            .then((response) => {
+                setState((state) => {
+                    ...state,
+                    loading: false,
+                    data: response.data
+                })
+    })
+        .catch((error) => {
+            setState((state) => {
+                    ...state,
+                loading: false,
+                error
+                })
+})
+    }, [axiosInstance, opts])
+
+return
 }
 
 const HookUseAxios = () => {
+    const { loading, error, data } = useAxios({
+        url: "https://yts.mx/api/v2/list_movies.json"
+    })
+    console.log(`Loading: ${loading}\nError: ${error}\nData: ${JSON.stringify(data)}`)
 
     return (
         <div>
-
+            <h1>Hello</h1>
         </div>
     )
 }
